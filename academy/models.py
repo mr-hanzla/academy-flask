97b4977999a9df1222b6f402e85a071c8a8ed4bd
Mr Hanzla <mr.hanzla101@gmail.com>
2022-07-27 14:07:41 +0500

    add User model

--- academy/models.py
+++ academy/models.py
@@ -1,6 +1,11 @@
-from flask_sqlalchemy import SQLAlchemy
 from academy import db
 
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password_hash = db.Column(db.String(70), nullable=False)
+    user_type = db.Column(db.String(), nullable=False)
 class PostTesting(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100), nullable=False)
