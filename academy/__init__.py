3edbfbfdc4985cc4649891a651d78a195bce5ede
Mr Hanzla <mr.hanzla101@gmail.com>
2022-07-27 14:07:06 +0500

    add secret key

--- academy/__init__.py
+++ academy/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///AcademyDatabase/academy.db'
+app.config['SECRET_KEY'] = 'c04be6db67e77e45c8feacf9'
 db = SQLAlchemy(app)
 
 from academy import routes
