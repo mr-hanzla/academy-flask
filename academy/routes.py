a9fa1dfe3162aeb565d15d3f14878a619e26bc8a
Mr Hanzla <mr.hanzla101@gmail.com>
2022-07-28 19:04:30 +0500

    add flash message error logic

--- academy/routes.py
+++ academy/routes.py
@@ -1,6 +1,9 @@
 from academy import app
-from flask import render_template
-from academy.forms import RegisterUserForm
+from flask import flash, render_template, redirect, url_for, request
+from academy.forms import RegisterUserForm, LoginUserForm
+from academy.models import User
+from academy import db
+from werkzeug.security import generate_password_hash
 
 def show(msg):
     print('*'*50)
@@ -9,11 +12,35 @@ def show(msg):
 
 @app.route('/')
 def root():
-    return render_template('index.html')
+    users = User.query.all()
+    return render_template('index.html', users=users)
 
-@app.route('/register')
+@app.route('/login', methods=['GET', 'POST'])
+def login_user():
+    form = LoginUserForm()
+    if request.method == 'POST':
+        return redirect(url_for('root'))
+    return render_template('academy/login.html', form=form)
+
+@app.route('/register', methods=['POST', 'GET'])
 def register_user():
     form = RegisterUserForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_user = User(
+                username=form.username.data,
+                email=form.email.data,
+                password_hash=form.password1.data,
+                user_type=form.user_type.data
+            )
+
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('root'))
+        if form.errors != {}:
+            for error in form.errors.values():
+                flash(f'FORM ERROR: {error}', category='danger')
+
     return render_template('academy/register.html', form=form)
 
 
