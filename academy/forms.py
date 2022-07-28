217e0280bae8e4e50ed110990d45a117e79d111b
Mr Hanzla <mr.hanzla101@gmail.com>
2022-07-28 19:03:02 +0500

    add user login form & update user registration form

--- academy/forms.py
+++ academy/forms.py
@@ -1,12 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField, SubmitField
+from wtforms import StringField, PasswordField, EmailField, SubmitField, SelectField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, AnyOf
+
+USER_TYPE_CHOICES = [
+    ('ADM', 'Admin'),
+    ('TCH', 'Teacher'),
+    ('STU', 'Student'),
+    ('MGM', 'Management'),
+]
 
 class RegisterUserForm(FlaskForm):
-    username = StringField(label='User Name')
-    email = EmailField(label='Email Address')
-    password1 = PasswordField(label='Password')
-    password2 = PasswordField(label='Conirm Password')
-    user_type = StringField(label='User Type')
+    username = StringField(label='User Name', validators=[Length(min=4, max=20), DataRequired()])
+    email = EmailField(label='Email Address', validators=[Email()])
+    password1 = PasswordField(label='Password', validators=[Length(min=6)])
+    password2 = PasswordField(label='Conirm Password', validators=[EqualTo('password1')])
+    user_type = SelectField(label='User Type', choices=USER_TYPE_CHOICES, validators=[DataRequired(), AnyOf(['admin', 'teacher', 'student', 'tadmin', 'sadmin', 'madmin'])])
     submit = SubmitField(label='Register User')
 
+class LoginUserForm(FlaskForm):
+    username = StringField(label='User Name')
+    password = PasswordField(label='Password')
+    submit = SubmitField(label='Sign in')
 
